7b945b513a5bd00772280f863ffff0942d145819
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2024-03-30 08:25:54 +0900

    feat: added external expression

---
+++ external-expr/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/cel-go/cel"
+)
+
+func main() {
+	// CEL環境の設定
+	env, err := cel.NewEnv(
+		// 'num' という名前の整数型の変数を宣言
+		cel.Variable("num", cel.IntType),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create CEL environment: %v", err)
+	}
+	// ローカルのテキストファイルを読み込み
+	b, err := os.ReadFile("./expr.txt")
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	// 外部から評価式を取得
+	expr := string(b)
+
+	// 式のコンパイル
+	ast, issues := env.Compile(expr)
+	if issues != nil && issues.Err() != nil {
+		log.Fatalf("Compile error: %v", issues.Err())
+	}
+
+	// プログラムの生成
+	prg, err := env.Program(ast)
+	if err != nil {
+		log.Fatalf("Program creation error: %v", err)
+	}
+
+	// 評価する入力値
+	inputs := map[string]interface{}{
+		"num": 10, // この値を変更して異なる入力で試すことができます
+	}
+
+	// プログラムの評価
+	result, _, err := prg.Eval(inputs)
+	if err != nil {
+		log.Fatalf("Evaluation error: %v", err)
+	}
+
+	// 結果の出力
+	fmt.Printf("Is %v an even number? %v\n", inputs["num"], result.Value().(bool))
+}
