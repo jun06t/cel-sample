31611872ed394610a184119f5438c7c2af9ad0b4
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2024-04-02 06:09:11 +0900

    chore: added comment

--- custom-function/main.go
+++ custom-function/main.go
@@ -24,9 +24,14 @@ func concatFunc(arg1, arg2 ref.Val) ref.Val {
 func global() {
 	env, err := cel.NewEnv(
 		cel.Function("concatStr",
+			// グローバルな関数なのでOverloadを使う
+			// 命名規則はfunc_argType_argType
 			cel.Overload("concatStr_string_string",
+				// 引数の型を指定
 				[]*cel.Type{cel.StringType, cel.StringType},
+				// 戻り値の型を指定
 				cel.StringType,
+				// 引数が２つの関数を用意するのでBinaryBinding
 				cel.BinaryBinding(concatFunc),
 			),
 		),
@@ -35,6 +40,7 @@ func global() {
 		log.Fatalf("failed to create env: %s\n", err)
 	}
 
+	// 評価式でカスタム関数を呼び出す。今回は変数なし
 	expr := `concatStr('Hello', 'World')`
 	ast, iss := env.Compile(expr)
 	if iss != nil && iss.Err() != nil {
@@ -46,6 +52,7 @@ func global() {
 		log.Fatalf("failed to create program: %s\n", err)
 	}
 
+	// 変数なしなので空のmapを渡す
 	out, _, err := p.Eval(map[string]interface{}{})
 	if err != nil {
 		log.Fatalf("evaluation error: %s\n", err)
@@ -56,12 +63,17 @@ func global() {
 
 func globalWithVariableArg(a, b string) {
 	env, err := cel.NewEnv(
+		// 引数を変数として定義
 		cel.Variable("arg1", cel.StringType),
 		cel.Variable("arg2", cel.StringType),
 		cel.Function("concatStr",
+			// グローバルな関数なのでOverloadを使う
 			cel.Overload("concatStr_string_string",
+				// 引数の型を指定
 				[]*cel.Type{cel.StringType, cel.StringType},
+				// 戻り値の型を指定
 				cel.StringType,
+				// 引数が２つの関数を用意するのでBinaryBinding
 				cel.BinaryBinding(concatFunc),
 			),
 		),
@@ -70,6 +82,7 @@ func globalWithVariableArg(a, b string) {
 		log.Fatalf("failed to create env: %s\n", err)
 	}
 
+	// 評価式でカスタム関数を呼び出す。今回は変数あり
 	expr := `concatStr(arg1, arg2)`
 	ast, iss := env.Compile(expr)
 	if iss != nil && iss.Err() != nil {
@@ -81,6 +94,7 @@ func globalWithVariableArg(a, b string) {
 		log.Fatalf("failed to create program: %s\n", err)
 	}
 
+	// 入力値として変数を渡す
 	out, _, err := p.Eval(map[string]interface{}{
 		"arg1": a,
 		"arg2": b,
@@ -94,12 +108,18 @@ func globalWithVariableArg(a, b string) {
 
 func memberWithVariableArg(a, b string) {
 	env, err := cel.NewEnv(
+		// メンバ、引数を変数として定義
+		cel.Variable("target", cel.StringType),
 		cel.Variable("arg1", cel.StringType),
-		cel.Variable("arg2", cel.StringType),
 		cel.Function("concatStr",
+			// メンバ関数の場合はMemberOverloadを使う
+			// 命名規則はtargetType_func_argType_argType
 			cel.MemberOverload("string_concatStr_string",
+				// 引数の型を指定。第一引数がメンバの型になる
 				[]*cel.Type{cel.StringType, cel.StringType},
+				// 戻り値の型を指定
 				cel.StringType,
+				// 引数が２つの関数を用意するのでBinaryBinding
 				cel.BinaryBinding(concatFunc),
 			),
 		),
@@ -108,7 +128,8 @@ func memberWithVariableArg(a, b string) {
 		log.Fatalf("failed to create env: %s\n", err)
 	}
 
-	expr := `arg1.concatStr(arg2)`
+	// メンバ関数の場合は、メンバに関数を生やして記述する
+	expr := `target.concatStr(arg1)`
 	ast, iss := env.Compile(expr)
 	if iss != nil && iss.Err() != nil {
 		log.Fatalf("failed to compile expression: %v", iss.Err())
@@ -120,8 +141,8 @@ func memberWithVariableArg(a, b string) {
 	}
 
 	out, _, err := p.Eval(map[string]interface{}{
-		"arg1": a,
-		"arg2": b,
+		"target": a,
+		"arg1":   b,
 	})
 	if err != nil {
 		log.Fatalf("evaluation error: %s\n", err)
